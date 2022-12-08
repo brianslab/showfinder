066598e20bac156e045c1e35f59db9f1b4e6aaae
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-08 03:55:27 -0800

    hook up tmdb

---
+++ api/tmdb.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+import { tmdbKey } from './keys';
+
+const searchTMDB = async () => {
+  const response = await axios.get('https://api.themoviedb.org/3/movie/76341', {
+    headers: {
+      Authorization: `Bearer ${tmdbKey.v4}`,
+    },
+    params: {},
+  });
+
+  console.log(response);
+
+  return response;
+};
+
+export default searchTMDB;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import searchTMDB from '../api/tmdb';
 
 export default function App({ Component, pageProps }: AppProps) {
+  searchTMDB();
+
   return <Component {...pageProps} />;
 }
