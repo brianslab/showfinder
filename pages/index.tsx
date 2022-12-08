4472f2e815ba1b48ca52c34ed70ceac4622f6ee5
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-08 01:50:05 -0800

    import button from udemy tutorial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,14 @@
+import Button from '../components/Button';
+
 export default function Home() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <div>
+        <Button primary outline rounded>
+          Custom Button
+        </Button>
+      </div>
+    </div>
+  );
 }
