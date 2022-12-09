3b7b05cf8d026d9b05e86f1e53da1525fd6b5a4c
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-09 01:31:44 -0800

    begin configuring search bar

--- api/tmdb.ts
+++ api/tmdb.ts
@@ -2,13 +2,16 @@ import axios from 'axios';
 
 import { tmdbKey } from './keys';
 
-const searchTMDB = async () => {
-  const response = await axios.get('https://api.themoviedb.org/3/movie/76341', {
-    headers: {
-      Authorization: `Bearer ${tmdbKey.v4}`,
-    },
-    params: {},
-  });
+const searchTMDB = async (term: string) => {
+  const response = await axios.get(
+    'https://api.themoviedb.org/3/search/multi',
+    {
+      headers: {
+        Authorization: `Bearer ${tmdbKey.v3}`,
+      },
+      params: { query: term },
+    }
+  );
 
   return response;
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import Button from '../components/Button';
+import SearchBar from '../components/SearchBar';
+import searchTMDB from '../api/tmdb';
 
 export default function Home() {
+  const handleSubmit = (term: string) => {
+    searchTMDB(term);
+  };
+
   return (
     <div>
       <h1 className='text-3xl font-bold underline'>Hello world!</h1>
@@ -8,6 +14,7 @@ export default function Home() {
         <Button primary outline rounded>
           Custom Button
         </Button>
+        <SearchBar onSubmit={handleSubmit} />
       </div>
     </div>
   );
