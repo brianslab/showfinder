e04e8f6cb17546f54fa1661a7391fea38bdf4c6f
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-21 04:32:16 -0800

    don't attempt to render posters if there aren't any

--- components/MediaList.tsx
+++ components/MediaList.tsx
@@ -7,9 +7,10 @@ interface MediaListProps {
 const MediaList: React.FC<MediaListProps> = ({ media }) => {
   const renderedMedia = media.map((medium: any) => {
     let poster = `https://image.tmdb.org/t/p/w500/${medium.poster_path}`;
+    let classes = medium.poster_path ? '' : 'hidden';
 
     return (
-      <div key={medium.id}>
+      <div key={medium.id} className={classes}>
         <MediaPosterShow title={medium.original_name} poster={poster} />
         {medium.name || medium.title}
       </div>
