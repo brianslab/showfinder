87a59765474b16e9beddac9aaaabce13176e8f39
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-21 04:44:39 -0800

    show posters in a grid

--- components/MediaList.tsx
+++ components/MediaList.tsx
@@ -7,17 +7,19 @@ interface MediaListProps {
 const MediaList: React.FC<MediaListProps> = ({ media }) => {
   const renderedMedia = media.map((medium: any) => {
     let poster = `https://image.tmdb.org/t/p/w500/${medium.poster_path}`;
-    let classes = medium.poster_path ? '' : 'hidden';
+    let exists = medium.poster_path ? '' : 'hidden';
 
     return (
-      <div key={medium.id} className={classes}>
+      <div key={medium.id} className={`${exists}`}>
         <MediaPosterShow title={medium.original_name} poster={poster} />
-        {medium.name || medium.title}
+        <div className='text-center text-white italic'>
+          {medium.name || medium.title}
+        </div>
       </div>
     );
   });
 
-  return <div>{renderedMedia}</div>;
+  return <div className='columns-5'>{renderedMedia}</div>;
 };
 
 export default MediaList;
