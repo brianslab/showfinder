58eb4e30684b6d5f5d344d29b4a49593cd61baad
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-30 02:08:44 -0800

    switch vercel_env to node_env

--- config/keys.ts
+++ config/keys.ts
@@ -2,7 +2,7 @@ import { keysType, tmdbKeyType } from '../types/keyTypes';
 
 let tmdbKey: tmdbKeyType;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.VERCEL_ENV === 'production') {
   tmdbKey = require('./prod').tmdbKey;
 } else {
   tmdbKey = require('./dev').tmdbKey;
