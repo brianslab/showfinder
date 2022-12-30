f1f31849ad062bb836d3ebd2c2559f5d4268f72a
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-30 02:18:14 -0800

    vercel public env

--- config/keys.ts
+++ config/keys.ts
@@ -2,7 +2,7 @@ import { keysType, tmdbKeyType } from '../types/keyTypes';
 
 let tmdbKey: tmdbKeyType;
 
-if (process.env.VERCEL_ENV === 'production') {
+if (process.env.VERCEL_PUBLIC_NODE_ENV === 'production') {
   tmdbKey = require('./prod').tmdbKey;
 } else {
   tmdbKey = require('./dev').tmdbKey;
