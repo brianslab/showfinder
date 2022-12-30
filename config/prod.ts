93957a293e228bcd2fdd19917715066ea2f64d22
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-30 02:20:38 -0800

    next public

--- config/prod.ts
+++ config/prod.ts
@@ -1,6 +1,6 @@
 import { tmdbKeyType } from '../types/keyTypes';
 
 export const tmdbKey: tmdbKeyType = {
-  v3: process.env.TMDB_V3,
-  v4: process.env.TMDB_V4,
+  v3: process.env.NEXT_PUBLIC_TMDB_V3,
+  v4: process.env.NEXT_PUBLIC_TMDB_V4,
 };
