46b9897881ced83d3ccba0f704882c248a9d586a
brianslab <bdkuhn602@gmail.com>
2023-01-15 22:45:05 -0800

    render release date

--- components/MediaList.tsx
+++ components/MediaList.tsx
@@ -6,15 +6,15 @@ interface MediaListProps {
 
 const MediaList: React.FC<MediaListProps> = ({ media }) => {
   const renderedMedia = media.map((medium: any) => {
-    let poster = `https://image.tmdb.org/t/p/w500/${medium.poster_path}`;
-    let exists = medium.poster_path ? '' : 'hidden';
+    const poster = `https://image.tmdb.org/t/p/w500/${medium.poster_path}`;
+    const exists = medium.poster_path ? '' : 'hidden';
 
     return (
       <div key={medium.id} className={exists}>
         <MediaPosterShow title={medium.original_name} poster={poster} />
         <div className='text-center text-white italic'>
-          {medium.name || medium.title} |{' '}
-          {medium.first_air_date || medium.release_date}
+          <p>{medium.name || medium.title}</p>
+          <p>{medium.first_air_date || medium.release_date}</p>
         </div>
       </div>
     );
