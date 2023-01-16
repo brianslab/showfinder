d2a8d4674ca7e72627682d5041edb0025e3c3b15
brianslab <bdkuhn602@gmail.com>
2023-01-15 20:33:01 -0800

    only search shows

--- pages/api/tmdb.ts
+++ pages/api/tmdb.ts
@@ -4,15 +4,12 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { keys } from '../../config/keys';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const response = await axios.get(
-    'https://api.themoviedb.org/3/search/multi',
-    {
-      headers: {
-        Authorization: `Bearer ${keys.tmdbKey.v4}`,
-      },
-      params: { query: req.body.term },
-    }
-  );
+  const response = await axios.get('https://api.themoviedb.org/3/search/tv', {
+    headers: {
+      Authorization: `Bearer ${keys.tmdbKey.v4}`,
+    },
+    params: { query: req.body.term },
+  });
 
   res.json(response.data.results);
 };
