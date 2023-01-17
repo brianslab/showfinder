e8fa1228a6d898ec00a49930204eb654bb9c2224
brianslab <bdkuhn602@gmail.com>
2023-01-17 01:25:41 -0800

    add title and classname props

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,9 +2,15 @@ import { useState } from 'react';
 
 interface SearchBarProps {
   onSubmit: (value: string) => void;
+  title?: string;
+  className?: string;
 }
 
-const SearchBar: React.FC<SearchBarProps> = ({ onSubmit }) => {
+const SearchBar: React.FC<SearchBarProps> = ({
+  onSubmit,
+  title,
+  className,
+}) => {
   const [term, setTerm] = useState('');
 
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -18,13 +24,16 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSubmit }) => {
   };
 
   return (
-    <div>
+    <div className={className}>
       <form onSubmit={handleFormSubmit}>
-        <input
-          value={term}
-          onChange={handleChange}
-          className='border-4 rounded border-red-900 focus:border-transparent focus:ring focus:ring-violet-300'
-        />
+        <h3 className='text-2xl text-white font-bold font-mono'>{title}</h3>
+        <div className={className}>
+          <input
+            value={term}
+            onChange={handleChange}
+            className='border-4 rounded border-red-900 focus:border-transparent focus:ring focus:ring-violet-300'
+          />
+        </div>
       </form>
     </div>
   );
