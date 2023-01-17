cdc2390edc8d234e4d1ad21088ee2a509ec00696
brianslab <bdkuhn602@gmail.com>
2023-01-17 05:36:24 -0800

    add modal for selection

--- components/MediaPosterShow.tsx
+++ components/MediaPosterShow.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 import Button from './Button';
+import Modal from './Modal';
 
 interface MediaPosterShowProps {
   poster: string;
@@ -6,16 +9,38 @@ interface MediaPosterShowProps {
   id: number;
 }
 
-const MediaPosterShow: React.FC<MediaPosterShowProps> = ({
-  poster,
-  title,
-  id,
-}) => {
-  return (
-    <Button bordered={false} onClick={() => console.log('clicked on', id)}>
-      <img src={poster} alt={title} />
-    </Button>
+function MediaPosterShow({ poster, title, id }: MediaPosterShowProps) {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <div>
+      <Button onClick={handleClose} primary>
+        Continue
+      </Button>
+    </div>
   );
-};
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>{title}</p>
+    </Modal>
+  );
+
+  return (
+    <div>
+      <Button bordered={false} onClick={handleClick}>
+        <img src={poster} alt={title} />
+      </Button>
+      {showModal && modal}
+    </div>
+  );
+}
 
 export default MediaPosterShow;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import '../styles/globals.css';
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const modalContainer = document.createElement('div');
+    modalContainer.classList.add('modal-container');
+    document.body.appendChild(modalContainer);
+
+    return () => {
+      document.body.removeChild(modalContainer);
+    };
+  }, []);
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
