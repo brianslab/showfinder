1357cb058e0eaf8ef8b8bb4fbe675ad7d4c43ea8
brianslab <bdkuhn602@gmail.com>
2023-01-17 04:34:59 -0800

    remove env checker variable

--- config/keys.ts
+++ config/keys.ts
@@ -1,11 +1,11 @@
 import { keysType, tmdbKeyType } from '../types/keyTypes';
 
 let tmdbKey: tmdbKeyType;
-const ON_VERCEL =
-  process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ||
-  process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview';
 
-if (ON_VERCEL === true) {
+if (
+  process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' ||
+  process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
+) {
   tmdbKey = require('./prod').tmdbKey;
 } else {
   tmdbKey = require('./dev').tmdbKey;
