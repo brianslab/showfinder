c64019c2a17ae3281bd69097861a45a411f565be
brianslab <bdkuhn602@gmail.com>
2023-01-17 01:44:15 -0800

    add border selector prop

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,6 +2,7 @@ import classnames from 'classnames';
 
 interface ButtonProps {
   children?: React.ReactNode;
+  bordered?: boolean;
   primary?: boolean;
   secondary?: boolean;
   success?: boolean;
@@ -14,6 +15,7 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({
   children,
+  bordered = true,
   primary,
   secondary,
   success,
@@ -23,24 +25,21 @@ const Button: React.FC<ButtonProps> = ({
   rounded,
   ...rest
 }) => {
-  const classes = classnames(
-    rest.className,
-    'flex items-center px-3 py-1.5 border',
-    {
-      'border-blue-500 bg-blue-500 text-white': primary,
-      'border-gray-900 bg-gray-900 text-white': secondary,
-      'border-green-500 bg-green-500 text-white': success,
-      'border-yellow-400 bg-yellow-400 text-white': warning,
-      'border-red-500 bg-red-500 text-white': danger,
-      'rounded-full': rounded,
-      'bg-white': outline,
-      'text-blue-500': outline && primary,
-      'text-gray-900': outline && secondary,
-      'text-green-500': outline && success,
-      'text-yellow-400': outline && warning,
-      'text-red-500': outline && danger,
-    }
-  );
+  const classes = classnames(rest.className, 'flex items-center px-3 py-1.5', {
+    border: bordered,
+    'border-blue-500 bg-blue-500 text-white': primary,
+    'border-gray-900 bg-gray-900 text-white': secondary,
+    'border-green-500 bg-green-500 text-white': success,
+    'border-yellow-400 bg-yellow-400 text-white': warning,
+    'border-red-500 bg-red-500 text-white': danger,
+    'rounded-full': rounded,
+    'bg-white': outline,
+    'text-blue-500': outline && primary,
+    'text-gray-900': outline && secondary,
+    'text-green-500': outline && success,
+    'text-yellow-400': outline && warning,
+    'text-red-500': outline && danger,
+  });
 
   return (
     <button {...rest} className={classes}>
