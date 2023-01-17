e205a6f60ade87073e1b1cc9db9603d7d39b62ea
brianslab <bdkuhn602@gmail.com>
2023-01-17 02:20:29 -0800

    created typed media array

--- components/MediaList.tsx
+++ components/MediaList.tsx
@@ -1,20 +1,21 @@
 import MediaPosterShow from './MediaPosterShow';
+import { media } from './ShowFinder';
 
 interface MediaListProps {
-  media: any;
+  media: media[];
 }
 
 const MediaList: React.FC<MediaListProps> = ({ media }) => {
-  const renderedMedia = media.map((medium: any) => {
+  const renderedMedia = media.map((medium: media) => {
     const poster = `https://image.tmdb.org/t/p/w500/${medium.poster_path}`;
     const exists = medium.poster_path ? '' : 'hidden';
 
     return (
       <div key={medium.id} className={exists}>
-        <MediaPosterShow title={medium.original_name} poster={poster} />
+        <MediaPosterShow title={medium.name} poster={poster} id={medium.id} />
         <div className='text-center text-white italic'>
-          <p>{medium.name || medium.title}</p>
-          <p>{medium.first_air_date || medium.release_date}</p>
+          <p>{medium.name}</p>
+          <p>{medium.date}</p>
         </div>
       </div>
     );
--- components/MediaPosterShow.tsx
+++ components/MediaPosterShow.tsx
@@ -3,11 +3,16 @@ import Button from './Button';
 interface MediaPosterShowProps {
   poster: string;
   title: string;
+  id: number;
 }
 
-const MediaPosterShow: React.FC<MediaPosterShowProps> = ({ poster, title }) => {
+const MediaPosterShow: React.FC<MediaPosterShowProps> = ({
+  poster,
+  title,
+  id,
+}) => {
   return (
-    <Button bordered={false}>
+    <Button bordered={false} onClick={() => console.log('clicked on', id)}>
       <img src={poster} alt={title} />
     </Button>
   );
